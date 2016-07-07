79c1c8f964ca5c521501f53e1fb96ad99fc4da26
Michael Richmond <mrichmond@infoblox.com>
2016-07-07 12:44:05 -0700

    Automating k8s namespace creation

--- middleware/kubernetes/test/20_setup_k8s_services.sh
+++ middleware/kubernetes/test/20_setup_k8s_services.sh
@@ -15,12 +15,25 @@ wait_until_k8s_ready() {
 		fi
 	done
 	echo "kubernetes nodes:"
-	$KUBECTL get nodes
+	${KUBECTL} get nodes
 }
+
+create_namespaces() {
+	for n in ${NAMESPACES};
+	do
+			echo "Creating namespace: ${n}"
+			${KUBECTL} get namespaces --no-headers 2>/dev/null | grep -q ${n}
+			if [ "${?}" != "0" ]; then
+				${KUBECTL} create namespace ${n}
+			fi
+	done
+
+	echo "kubernetes namespaces:"
+	${KUBECTL} get namespaces
+}
+
 
 wait_until_k8s_ready
 
-
-echo "all cool"
-
-
+NAMESPACES="demo test"
+create_namespaces
