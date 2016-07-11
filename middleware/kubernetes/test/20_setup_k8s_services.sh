1810b7b06d43108b5c2ef4f216304aa3f3c2ea29
Michael Richmond <mrichmond@infoblox.com>
2016-07-11 11:47:41 -0700

    Adding automation to start 4 k8s services

--- middleware/kubernetes/test/20_setup_k8s_services.sh
+++ middleware/kubernetes/test/20_setup_k8s_services.sh
@@ -32,8 +32,49 @@ create_namespaces() {
 	${KUBECTL} get namespaces
 }
 
+# run_and_expose_service <servicename> <namespace> <image> <port>
+run_and_expose_service() {
+
+    if [ "${#}" != "4" ]; then
+        return -1
+    fi
+
+    service="${1}"
+    namespace="${2}"
+    image="${3}"
+    port="${4}"
+
+    echo "   starting service '${service}' in namespace '${namespace}"
+
+    ${KUBECTL} get deployment --namespace=${namespace} --no-headers 2>/dev/null | grep -q ${service}
+    if [ "${?}" != "0" ]; then
+        ${KUBECTL} run ${service} --namespace=${namespace} --image=${image}
+    else
+        echo "warn: service '${service}' already running in namespace '${namespace}'"
+    fi
+
+    ${KUBECTL} get service --namespace=${namespace} --no-headers 2>/dev/null | grep -q ${service}
+    if [ "${?}" != "0" ]; then
+        ${KUBECTL} expose deployment ${service} --namespace=${namespace} --port=${port}
+    else
+        echo "warn: service '${service}' already exposed in namespace '${namespace}'"
+    fi
+}
+
 
 wait_until_k8s_ready
 
 NAMESPACES="demo test"
 create_namespaces
+
+echo ""
+echo "Starting services:"
+
+run_and_expose_service mynginx demo nginx 80
+run_and_expose_service webserver demo nginx 80
+run_and_expose_service mynginx test nginx 80
+run_and_expose_service webserver test nginx 80
+
+echo ""
+echo "Services exposed:"
+${KUBECTL} get services --all-namespaces
