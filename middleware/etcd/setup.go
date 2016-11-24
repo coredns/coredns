3d1063209ae7179decc4663fa776bc53680e9541
Miek Gieben <miek@miek.nl>
2016-11-24 15:36:15 +0000

    Fix the other upstream parse as well

--- middleware/etcd/setup.go
+++ middleware/etcd/setup.go
@@ -132,13 +132,11 @@ func etcdParse(c *caddy.Controller) (*Etcd, bool, error) {
 						if len(args) == 0 {
 							return &Etcd{}, false, c.ArgErr()
 						}
-						for i := 0; i < len(args); i++ {
-							h, p, e := net.SplitHostPort(args[i])
-							if e != nil && p == "" {
-								args[i] = h + ":53"
-							}
+						ups, err := dnsutil.ParseHostPortOrFile(args...)
+						if err != nil {
+							return &Etcd{}, false, c.ArgErr()
 						}
-						etc.Proxy = proxy.New(args)
+						etc.Proxy = proxy.New(ups)
 					case "tls": // cert key cacertfile
 						args := c.RemainingArgs()
 						if len(args) != 3 {
