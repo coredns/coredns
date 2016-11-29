0996b19622bb5634dd54edbf42fd1e474f2fea59
Miek Gieben <miek@miek.nl>
2016-11-29 09:34:42 +0000

    Testing cleanups

--- middleware/pkg/debug/debug_test.go
+++ middleware/pkg/debug/debug_test.go
@@ -6,16 +6,16 @@ import (
 )
 
 func TestIsDebug(t *testing.T) {
-	if ok := isDebug("o-o.debug.miek.nl."); ok != "miek.nl." {
+	if ok := IsDebug("o-o.debug.miek.nl."); ok != "miek.nl." {
 		t.Errorf("expected o-o.debug.miek.nl. to be debug")
 	}
-	if ok := isDebug(strings.ToLower("o-o.Debug.miek.nl.")); ok != "miek.nl." {
+	if ok := IsDebug(strings.ToLower("o-o.Debug.miek.nl.")); ok != "miek.nl." {
 		t.Errorf("expected o-o.Debug.miek.nl. to be debug")
 	}
-	if ok := isDebug("i-o.debug.miek.nl."); ok != "" {
+	if ok := IsDebug("i-o.debug.miek.nl."); ok != "" {
 		t.Errorf("expected i-o.Debug.miek.nl. to be non-debug")
 	}
-	if ok := isDebug(strings.ToLower("i-o.Debug.")); ok != "" {
+	if ok := IsDebug(strings.ToLower("i-o.Debug.")); ok != "" {
 		t.Errorf("expected o-o.Debug. to be non-debug")
 	}
 }
