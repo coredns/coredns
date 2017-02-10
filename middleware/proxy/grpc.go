e72b0ea107375eeb7c067dcf1719e04349381ef2
John Belamaric <jbelamaric@infoblox.com>
2017-02-10 16:10:22 +0000

    Implement OnShutdown; add a little logging

--- middleware/proxy/grpc.go
+++ middleware/proxy/grpc.go
@@ -16,6 +16,7 @@ import (
 type grpcClient struct {
 	dialOpt  grpc.DialOption
 	clients  map[string]pb.DnsServiceClient
+	conns    []*grpc.ClientConn
 	upstream *staticUpstream
 }
 
@@ -51,15 +52,27 @@ func (g *grpcClient) Exchange(addr string, state request.Request) (*dns.Msg, err
 }
 
 func (g *grpcClient) Protocol() string          { return "grpc" }
-func (g *grpcClient) OnShutdown(p *Proxy) error { return nil }
+
+func (g *grpcClient) OnShutdown(p *Proxy) error {
+	for i, conn := range g.conns {
+		log.Printf("[INFO] Closing connection %d\n", i)
+		err := conn.Close()
+		if err != nil {
+			log.Printf("[WARNING] Error closing connection %d: %s\n", i, err)
+		}
+	}
+	return nil
+}
 
 func (g *grpcClient) OnStartup(p *Proxy) error {
-	for _, host := range g.upstream.Hosts {
+	for i, host := range g.upstream.Hosts {
+		log.Printf("[INFO] Creating gRPC connection %d to %s.\n", i, host.Name)
 		conn, err := grpc.Dial(host.Name, g.dialOpt)
 		if err != nil {
-			log.Printf("[WARNING] Skipping gRPC host '%s' due to Dial error: '%s'", host, err)
+			log.Printf("[WARNING] Skipping gRPC host '%s' due to Dial error: %s\n", host.Name, err)
 		} else {
 			g.clients[host.Name] = pb.NewDnsServiceClient(conn)
+			g.conns = append(g.conns, conn)
 		}
 	}
 	return nil
