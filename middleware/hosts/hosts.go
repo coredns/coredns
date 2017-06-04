9df130b73355f9716373a56db71fcce0ace7968b
Pat Moroney <pmoroney@name.com>
2017-06-04 01:39:12 -0600

    refactored the logic for creating records and filtering ip address versions. also got PTR lookups working

--- middleware/hosts/hosts.go
+++ middleware/hosts/hosts.go
@@ -26,65 +26,36 @@ func (h Hosts) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (
 	}
 	qname := state.Name()
 
-	// Now the real zone.
-	zone := middleware.Zones(h.Names()).Matches(qname)
-
 	answers := []dns.RR{}
 
-	switch state.QType() {
-	case dns.TypePTR:
+	zone := middleware.Zones(h.Names()).Matches(qname)
+	if zone == "" {
+		if state.Type() != "PTR" {
+			return middleware.NextOrFailure(h.Name(), h.Next, ctx, w, r)
+		}
+
+		// Request is a PTR
+		zone = state.Name()
 		names := h.LookupStaticAddr(dnsutil.ExtractAddressFromReverse(zone))
 		if len(names) == 0 {
 			return middleware.NextOrFailure(h.Name(), h.Next, ctx, w, r)
 		}
-		for _, n := range names {
-			r := new(dns.PTR)
-			r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypePTR,
-				Class: dns.ClassINET, Ttl: 3600}
-			r.Ptr = n
-			answers = append(answers, r)
+		answers = ptr(zone, names)
+	}
+
+	if zone != "" {
+		ips := h.LookupStaticHost(zone)
+
+		switch state.QType() {
+		case dns.TypeA:
+			answers = a(zone, ips)
+		case dns.TypeAAAA:
+			answers = aaaa(zone, ips)
 		}
-	case dns.TypeA:
-		strings := h.LookupStaticHost(zone)
-		if len(strings) == 0 {
-			return middleware.NextOrFailure(h.Name(), h.Next, ctx, w, r)
-		}
-		for _, str := range strings {
-			ip := net.ParseIP(str)
-			if ip == nil {
-				continue
-			}
-			ip = ip.To4()
-			if ip == nil {
-				continue
-			}
-			r := new(dns.A)
-			r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeA,
-				Class: dns.ClassINET, Ttl: 3600}
-			r.A = ip
-			answers = append(answers, r)
-		}
-	case dns.TypeAAAA:
-		strings := h.LookupStaticHost(zone)
-		if len(strings) == 0 {
-			return middleware.NextOrFailure(h.Name(), h.Next, ctx, w, r)
-		}
-		for _, str := range strings {
-			ip := net.ParseIP(str)
-			if ip == nil {
-				continue
-			}
-			ipv4 := ip.To4()
-			if ipv4 != nil {
-				continue
-			}
-			r := new(dns.AAAA)
-			r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeAAAA,
-				Class: dns.ClassINET, Ttl: 3600}
-			r.AAAA = ip
-			answers = append(answers, r)
-		}
-	default:
+	}
+
+	if len(answers) == 0 {
+		return middleware.NextOrFailure(h.Name(), h.Next, ctx, w, r)
 	}
 
 	m := new(dns.Msg)
@@ -99,3 +70,71 @@ func (h Hosts) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (
 }
 
 func (h Hosts) Name() string { return "hosts" }
+
+func ipv4Filter(strings []string) []net.IP {
+	ips := []net.IP{}
+	for _, str := range strings {
+		ip := net.ParseIP(str)
+		if ip == nil {
+			continue
+		}
+		ip = ip.To4()
+		if ip == nil {
+			continue
+		}
+		ips = append(ips, ip)
+	}
+	return ips
+}
+
+func ipv6Filter(strings []string) []net.IP {
+	ips := []net.IP{}
+	for _, str := range strings {
+		ip := net.ParseIP(str)
+		if ip == nil {
+			continue
+		}
+		ipv4 := ip.To4()
+		if ipv4 != nil {
+			continue
+		}
+		ips = append(ips, ip)
+	}
+	return ips
+}
+
+func a(zone string, ips []string) []dns.RR {
+	answers := []dns.RR{}
+	for _, ip := range ipv4Filter(ips) {
+		r := new(dns.A)
+		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeA,
+			Class: dns.ClassINET, Ttl: 3600}
+		r.A = ip
+		answers = append(answers, r)
+	}
+	return answers
+}
+
+func aaaa(zone string, ips []string) []dns.RR {
+	answers := []dns.RR{}
+	for _, ip := range ipv6Filter(ips) {
+		r := new(dns.AAAA)
+		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeAAAA,
+			Class: dns.ClassINET, Ttl: 3600}
+		r.AAAA = ip
+		answers = append(answers, r)
+	}
+	return answers
+}
+
+func ptr(zone string, names []string) []dns.RR {
+	answers := []dns.RR{}
+	for _, n := range names {
+		r := new(dns.PTR)
+		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypePTR,
+			Class: dns.ClassINET, Ttl: 3600}
+		r.Ptr = dns.Fqdn(n)
+		answers = append(answers, r)
+	}
+	return answers
+}
