28c3b35e16ebc3c5d702d5d7e024c484cc990235
Miek Gieben <miek@miek.nl>
2017-06-13 22:43:59 +0000

    Feedback: check the value of Debug

--- middleware/debug/debug_test.go
+++ middleware/debug/debug_test.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"testing"
 
+	"github.com/coredns/coredns/core/dnsserver"
+
 	"github.com/mholt/caddy"
 )
 
@@ -12,31 +14,36 @@ func TestDebug(t *testing.T) {
 	log.SetOutput(ioutil.Discard)
 
 	tests := []struct {
-		input     string
-		shouldErr bool
+		input         string
+		shouldErr     bool
+		expectedDebug bool
 	}{
 		// positive
 		{
-			`debug`, false,
+			`debug`, false, true,
 		},
 		// negative
 		{
-			`debug off`, true,
+			`debug off`, true, false,
 		},
 	}
 
 	for i, test := range tests {
 		c := caddy.NewTestController("dns", test.input)
 		err := setup(c)
+		cfg := dnsserver.GetConfig(c)
 
 		if test.shouldErr && err == nil {
-			t.Errorf("Test %d: Expected error but found %s for input %s", i, err, test.input)
+			t.Fatalf("Test %d: Expected error but found %s for input %s", i, err, test.input)
 		}
 
 		if err != nil {
 			if !test.shouldErr {
-				t.Errorf("Test %d: Expected no error but found one for input %s. Error was: %v", i, test.input, err)
+				t.Fatalf("Test %d: Expected no error but found one for input %s. Error was: %v", i, test.input, err)
 			}
+		}
+		if cfg.Debug != test.expectedDebug {
+			t.Fatalf("Test %d: Expected debug to be: %t, but got: %t, input: %s", i, test.expectedDebug, test.input)
 		}
 	}
 }
