484b015490c1642d7917862fb7d08d5c4cea8a4f
ghostflame <cybernetic.dragon@gmail.com>
2017-06-26 22:25:08 +0100

    Removes debug message

--- middleware/proxy/google.go
+++ middleware/proxy/google.go
@@ -242,7 +242,6 @@ func newUpstream(hosts []string, old *staticUpstream) Upstream {
 			WithoutPathPrefix: upstream.WithoutPathPrefix,
 		}
 
-		log.Printf("[DEBUG] Ggl: Host %s marked healthy until %s.\n", uh.Name, uh.OkUntil.Local())
 		upstream.Hosts[i] = uh
 	}
 	return upstream
