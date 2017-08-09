e4088104da0bd664f194577b1d2ae92023fac1ce
Miek Gieben <miek@miek.nl>
2017-08-09 09:17:17 +0100

    cleanup chaos

--- middleware/chaos/chaos.go
+++ middleware/chaos/chaos.go
@@ -54,10 +54,6 @@ func (c Chaos) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (
 // Name implements the Handler interface.
 func (c Chaos) Name() string { return "chaos" }
 
-func (c Chaos) AutoPath(state request.Request) ([]string, error) {
-	return []string{"boe", "bah", c.Version, ""}, nil
-}
-
 func trim(s string) string {
 	if len(s) < 256 {
 		return s
