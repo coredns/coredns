eb3d64f06fde15d3ef028f4b76c5b31c39b0babc
Miek Gieben <miek@miek.nl>
2018-04-01 12:39:44 +0100

    more docs

--- plugin/metrics/context.go
+++ plugin/metrics/context.go
@@ -6,9 +6,15 @@ import (
 	"golang.org/x/net/context"
 )
 
-// WithServer returns the current server handling the request.
-// It returns the server listening address which is something like "dns://:53".
-// TODO(miek): take bind into account.
+// WithServer returns the current server handling the request. It returns the
+// server listening address: <scheme>://[<bind>]:<port> Normally this is
+// something like "dns://:53", but if the bind plugin is used, i.e. "bind
+// 127.0.0.53", it will be "dns://127.0.0.53:53", etc. If not address is found
+// the empty string is returned.
+//
+// Basic usage with a metric:
+//
+// <metric>.WithLabelValues(metrics.WithServer(ctx), labels..).Add(1)
 func WithServer(ctx context.Context) string {
 	srv := ctx.Value(plugin.ServerCtx{})
 	if srv == nil {
