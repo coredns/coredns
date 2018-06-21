9b760a818522f8884a8f2d74e1920ac0fc48a577
Chris O'Haver <cohaver@infoblox.com>
2018-06-21 10:56:36 -0400

    add integration test

--- plugin/template/template_test.go
+++ plugin/template/template_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	gotmpl "text/template"
 
+	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin/pkg/dnstest"
 	"github.com/coredns/coredns/plugin/pkg/fall"
 	"github.com/coredns/coredns/plugin/test"
