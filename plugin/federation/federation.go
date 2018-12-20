7d038d88e8f384ca0561f6a74b0ed39dbb19be97
Björn Kinscher <mail@bjoern-kinscher.de>
2018-12-20 15:04:57 +0100

    removed more unnecessary assignments

--- plugin/federation/federation.go
+++ plugin/federation/federation.go
@@ -104,7 +104,7 @@ func (f *Federation) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.
 
 	m := new(dns.Msg)
 	m.SetReply(r)
-	m.Authoritative, m.RecursionAvailable = true, false
+	m.Authoritative = true
 
 	m.Answer = []dns.RR{service.NewCNAME(state.QName(), service.Host)}
 
