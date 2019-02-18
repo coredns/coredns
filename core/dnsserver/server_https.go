80e0b0b6bb948a6f35cc19ab1308733f262d7812
Andrey Meshkov <ay.meshkov@gmail.com>
2019-02-18 11:03:02 +0300

    Fixing panic in case if there's no response
    There could be a situation when there's no response after ServeDNS call. With the current implementation, this leads to panic.

--- core/dnsserver/server_https.go
+++ core/dnsserver/server_https.go
@@ -119,6 +119,11 @@ func (s *ServerHTTPS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// We should expect a packet to be returned that we can send to the client.
 	s.ServeDNS(context.Background(), dw, msg)
 
+	if dw.Msg == nil {
+		http.Error(w, "No response", http.StatusInternalServerError)
+		return
+	}
+
 	buf, _ := dw.Msg.Pack()
 
 	mt, _ := response.Typify(dw.Msg, time.Now().UTC())
