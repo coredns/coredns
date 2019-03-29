64b6b6e7bf1d2504444fe12eb6f9670c7e170682
Miek Gieben <miek@miek.nl>
2019-03-29 16:33:12 +0000

    Fiddle with the tests a bit
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/forward/persistent_test.go
+++ plugin/forward/persistent_test.go
@@ -57,77 +57,29 @@ func TestCleanupByTimer(t *testing.T) {
 	defer s.Close()
 
 	tr := newTransport(s.Addr)
-	tr.SetExpire(100 * time.Millisecond)
+	tr.SetExpire(100 * time.Microsecond)
 	tr.Start()
 	defer tr.Stop()
 
 	c1, _, _ := tr.Dial("udp")
 	c2, _, _ := tr.Dial("udp")
 	tr.Yield(c1)
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(40 * time.Microsecond)
 	tr.Yield(c2)
 
-	time.Sleep(120 * time.Millisecond)
+	time.Sleep(140 * time.Microsecond)
 	c3, cached, _ := tr.Dial("udp")
 	if cached {
 		t.Error("Expected non-cached connection (c3)")
 	}
 	tr.Yield(c3)
 
-	time.Sleep(120 * time.Millisecond)
+	time.Sleep(160 * time.Microsecond)
 	c4, cached, _ := tr.Dial("udp")
 	if cached {
 		t.Error("Expected non-cached connection (c4)")
 	}
 	tr.Yield(c4)
-}
-
-func TestPartialCleanup(t *testing.T) {
-	s := dnstest.NewServer(func(w dns.ResponseWriter, r *dns.Msg) {
-		ret := new(dns.Msg)
-		ret.SetReply(r)
-		w.WriteMsg(ret)
-	})
-	defer s.Close()
-
-	tr := newTransport(s.Addr)
-	tr.SetExpire(100 * time.Millisecond)
-	tr.Start()
-	defer tr.Stop()
-
-	c1, _, _ := tr.Dial("udp")
-	c2, _, _ := tr.Dial("udp")
-	c3, _, _ := tr.Dial("udp")
-	c4, _, _ := tr.Dial("udp")
-	c5, _, _ := tr.Dial("udp")
-
-	tr.Yield(c1)
-	time.Sleep(10 * time.Millisecond)
-	tr.Yield(c2)
-	time.Sleep(10 * time.Millisecond)
-	tr.Yield(c3)
-	time.Sleep(50 * time.Millisecond)
-	tr.Yield(c4)
-	time.Sleep(10 * time.Millisecond)
-	tr.Yield(c5)
-	time.Sleep(40 * time.Millisecond)
-
-	c6, _, _ := tr.Dial("udp")
-	if c6 != c5 {
-		t.Errorf("Expected c6 == c5")
-	}
-	c7, _, _ := tr.Dial("udp")
-	if c7 != c4 {
-		t.Errorf("Expected c7 == c4")
-	}
-	c8, cached, _ := tr.Dial("udp")
-	if cached {
-		t.Error("Expected non-cached connection (c8)")
-	}
-
-	tr.Yield(c6)
-	tr.Yield(c7)
-	tr.Yield(c8)
 }
 
 func TestCleanupAll(t *testing.T) {
