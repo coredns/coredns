f7f48a96896a9c2497d409611019f7ecf995c104
Andrey Meshkov <am@adguard.com>
2019-05-24 17:41:23 +0300

    use s.Server instead of s

--- core/dnsserver/server_https.go
+++ core/dnsserver/server_https.go
@@ -117,7 +117,7 @@ func (s *ServerHTTPS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	// We just call the normal chain handler - all error handling is done there.
 	// We should expect a packet to be returned that we can send to the client.
-	ctx := context.WithValue(context.Background(), Key{}, s)
+	ctx := context.WithValue(context.Background(), Key{}, s.Server)
 	s.ServeDNS(ctx, dw, msg)
 
 	// See section 4.2.1 of RFC 8484.
