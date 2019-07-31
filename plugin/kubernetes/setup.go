238a7d93444db558313ed83b46024750cfd89795
Miek Gieben <miek@miek.nl>
2019-07-31 20:38:56 +0100

    "plugin/kubernetes: remove some of the klog setup (#3054)"
    This reverts commit 4fda9535d2f446de9dbaf8b397b0d871bdc882e5.
    
    This uncovers another race that we didn't have before; color me
    confused.

--- plugin/kubernetes/setup.go
+++ plugin/kubernetes/setup.go
@@ -2,6 +2,7 @@ package kubernetes
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
@@ -35,7 +36,15 @@ import (
 var log = clog.NewWithPlugin("kubernetes")
 
 func init() {
-	klog.SetOutput(os.Stdout)
+	// Kubernetes plugin uses the kubernetes library, which now uses klog, we must set and parse this flag
+	// so we don't log to the filesystem, which can fill up and crash CoreDNS indirectly by calling os.Exit().
+	// We also set: os.Stderr = os.Stdout in the setup function below so we output to standard out; as we do for
+	// all CoreDNS logging. We can't do *that* in the init function, because we, when starting, also barf some
+	// things to stderr.
+	klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
+	klog.InitFlags(klogFlags)
+	logtostderr := klogFlags.Lookup("logtostderr")
+	logtostderr.Value.Set("true")
 
 	caddy.RegisterPlugin("kubernetes", caddy.Plugin{
 		ServerType: "dns",
@@ -44,6 +53,9 @@ func init() {
 }
 
 func setup(c *caddy.Controller) error {
+	// See comment in the init function.
+	os.Stderr = os.Stdout
+
 	k, err := kubernetesParse(c)
 	if err != nil {
 		return plugin.Error("kubernetes", err)
