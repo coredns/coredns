fb79a4fa5f17a4e64c1844d46c92b5d3895dcaf8
Chris O'Haver <cohaver@infoblox.com>
2020-05-12 16:45:08 -0400

    check for nil
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- plugin/kubernetes/object/informer.go
+++ plugin/kubernetes/object/informer.go
@@ -1,13 +1,16 @@
 package object
 
 import (
+	"errors"
+	"reflect"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/cache"
 )
 
 // NewIndexerInformer is a copy of the cache.NewIndexerInformer function, but allows custom process function
 func NewIndexerInformer(lw cache.ListerWatcher, objType runtime.Object, h cache.ResourceEventHandler, indexers cache.Indexers, builder ProcessorBuilder) (cache.Indexer, cache.Controller) {
-	clientState := cache.NewIndexer(cache.DeletionHandlingMetaNamespaceKeyFunc, indexers)
+	clientState := cache.NewIndexer(deletionHandlingMetaNamespaceKeyFunc, indexers)
 
 	cfg := &cache.Config{
 		Queue:            cache.NewDeltaFIFO(cache.MetaNamespaceKeyFunc, clientState),
@@ -18,6 +21,13 @@ func NewIndexerInformer(lw cache.ListerWatcher, objType runtime.Object, h cache.
 		Process:          builder(clientState, h),
 	}
 	return clientState, cache.New(cfg)
+}
+
+func deletionHandlingMetaNamespaceKeyFunc(obj interface{})(string, error){
+	if obj == nil || reflect.ValueOf(obj).IsNil() {
+		return "", errors.New("nil object")
+	}
+	return cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
 }
 
 // DefaultProcessor is a copy of Process function from cache.NewIndexerInformer except it does a conversion.
