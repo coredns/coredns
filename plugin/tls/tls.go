200e086c4a7092a42c471444a5e92acdae02e441
OleksandrBlack <aleksandr.chornuy@gmail.com>
2020-07-24 08:33:02 +0300

    Add support for five TLSv1.3 ciphersuites

--- plugin/tls/tls.go
+++ plugin/tls/tls.go
@@ -33,6 +33,11 @@ func setTLSDefaults(tls *ctls.Config) {
 		ctls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 		ctls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 		ctls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		ctls.TLS_AES_256_GCM_SHA384,
+		ctls.TLS_CHACHA20_POLY1305_SHA256,
+		ctls.TLS_AES_128_GCM_SHA256,
+		ctls.TLS_AES_128_CCM_8_SHA256,
+		ctls.TLS_AES_128_CCM_SHA256,
 	}
 	tls.PreferServerCipherSuites = true
 }
