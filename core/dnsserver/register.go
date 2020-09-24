647ff561bba1e2125540e4efeffc78217d1f04f5
Miek Gieben <miek@miek.nl>
2020-09-24 11:17:20 +0200

    Fix imports
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- core/dnsserver/register.go
+++ core/dnsserver/register.go
@@ -7,13 +7,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/coredns/caddy"
+	"github.com/coredns/caddy/caddyfile"
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/pkg/dnsutil"
 	"github.com/coredns/coredns/plugin/pkg/parse"
 	"github.com/coredns/coredns/plugin/pkg/transport"
-
-	"github.com/coredns/caddy"
-	"github.com/coredns/caddy/caddyfile"
 )
 
 const serverType = "dns"
