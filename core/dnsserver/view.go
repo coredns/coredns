1df1ac0ad881d9c96dce1b5f2b50b063b527d4d9
Chris O'Haver <cohaver@infoblox.com>
2021-02-12 13:48:29 -0500

    doc Viewer interface
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- core/dnsserver/view.go
+++ core/dnsserver/view.go
@@ -2,6 +2,8 @@ package dnsserver
 
 import "github.com/coredns/coredns/request"
 
+// Viewer - If Viewer is implemented by a registered plugin in a server, its Filter()
+// is added to the server's filterFuncs.
 type Viewer interface {
 	Filter(request.Request) bool
 }
