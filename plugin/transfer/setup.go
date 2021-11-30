fd0301b1a3dde2087af1cb7382feaa242cbeade3
Chris O'Haver <cohaver@infoblox.com>
2021-11-30 16:52:14 -0500

    fix caddy pkg import grouping
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- plugin/transfer/setup.go
+++ plugin/transfer/setup.go
@@ -2,7 +2,6 @@ package transfer
 
 import (
 	"github.com/coredns/caddy"
-
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/pkg/parse"
