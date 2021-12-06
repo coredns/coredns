23dda34309bca772a6a7a527ca6e67c02818b90e
Chris O'Haver <cohaver@infoblox.com>
2021-12-06 14:35:28 -0500

    Add BADSIG test
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- test/tsig_test.go
+++ test/tsig_test.go
@@ -58,6 +58,7 @@ func TestTsigBadKey(t *testing.T) {
 	m.SetQuestion("test.", dns.TypeA)
 	m.SetTsig("bad.key.", dns.HmacSHA256, 300, time.Now().Unix())
 
+	// rename client key to a key name the server doesnt have
 	client := dns.Client{Net: "udp", TsigSecret: map[string]string{"bad.key.": tsigSecret}}
 	r, _, err := client.Exchange(m, udp)
 
@@ -85,6 +86,43 @@ func TestTsigBadKey(t *testing.T) {
 	}
 }
 
-// TODO: TestTsigBadSig
+func TestTsigBadSig(t *testing.T) {
+	i, udp, _, err := CoreDNSServerAndPorts(corefile)
+	if err != nil {
+		t.Fatalf("Could not get CoreDNS serving instance: %s", err)
+	}
+	defer i.Stop()
+
+	m := new(dns.Msg)
+	m.SetQuestion("test.", dns.TypeA)
+	m.SetTsig(tsigKey, dns.HmacSHA256, 300, time.Now().Unix())
+
+	// mangle the client secret so the sig wont match the server sig
+	client := dns.Client{Net: "udp", TsigSecret: map[string]string{tsigKey: "BADSIG00ECfVZG2qCjr4mPpaGim/Bq+IWMiNrLjUO4Y="}}
+	r, _, err := client.Exchange(m, udp)
+
+	if err != dns.ErrAuth {
+		t.Fatalf("Expected \"dns: bad authentication\" error, got: %s", err)
+	}
+	if r.Rcode != dns.RcodeNotAuth {
+		t.Fatalf("Rcode should be dns.RcodeNotAuth")
+	}
+	tsig := r.IsTsig()
+	if tsig == nil {
+		t.Fatalf("Respose was not TSIG")
+	}
+	if tsig.Error != dns.RcodeBadSig {
+		t.Fatalf("TSIG Error code should be dns.RcodeBadSig")
+	}
+	if tsig.MAC != "" {
+		t.Fatalf("TSIG MAC should be empty")
+	}
+	if tsig.MACSize != 0 {
+		t.Fatalf("TSIG MACSize should be 0")
+	}
+	if tsig.TimeSigned != 0 {
+		t.Fatalf("TSIG TimeSigned should be 0")
+	}
+}
 
 // TODO: TestTsigBadTime
