07a2918001a5479c36237202858bf1c583bfa7ee
Marius Kimmina <mar.kimmina@gmail.com>
2022-07-15 09:59:08 +0200

    adjust test structure as requested
    Signed-off-by: Marius Kimmina <mar.kimmina@gmail.com>

--- test/tls_test.go
+++ test/tls_test.go
@@ -8,55 +8,39 @@ import (
 )
 
 func TestDNSoverTLS(t *testing.T) {
-	testcases := []struct {
-		name         string
-		config       string
-		Qname        string
-		Qtype        uint16
-		Answer       []dns.RR
-		AnswerLength int
-	}{
-		{
-			name: "Test whoami with DNS over TLS",
-			config: `tls://.:1053 {
-                tls ../plugin/tls/test_cert.pem ../plugin/tls/test_key.pem
-                whoami
-            }`,
-			Qname:        "example.com.",
-			Qtype:        dns.TypeA,
-			Answer:       []dns.RR{},
-			AnswerLength: 0,
-		},
+	corefile := `tls://.:1053 {
+        tls ../plugin/tls/test_cert.pem ../plugin/tls/test_key.pem
+        whoami
+    }`
+	qname := "example.com."
+	qtype := dns.TypeA
+	answerLength := 0
+
+	ex, _, tcp, err := CoreDNSServerAndPorts(corefile)
+	if err != nil {
+		t.Fatalf("Could not get CoreDNS serving instance: %s", err)
 	}
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			ex, _, tcp, err := CoreDNSServerAndPorts(tc.config)
-			if err != nil {
-				t.Fatalf("Could not get CoreDNS serving instance: %s", err)
-			}
-			defer ex.Stop()
+	defer ex.Stop()
 
-			m := new(dns.Msg)
-			m.SetQuestion(tc.Qname, tc.Qtype)
-			client := dns.Client{
-				Net:       "tcp-tls",
-				TLSConfig: &tls.Config{InsecureSkipVerify: true},
-			}
-			r, _, err := client.Exchange(m, tcp)
+	m := new(dns.Msg)
+	m.SetQuestion(qname, qtype)
+	client := dns.Client{
+		Net:       "tcp-tls",
+		TLSConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	r, _, err := client.Exchange(m, tcp)
 
-			if err != nil {
-				t.Fatalf("Could not exchange msg: %s", err)
-			}
+	if err != nil {
+		t.Fatalf("Could not exchange msg: %s", err)
+	}
 
-			if n := len(r.Answer); n != tc.AnswerLength {
-				t.Fatalf("Expected %v answers, got %v", tc.AnswerLength, n)
-			}
-			if n := len(r.Extra); n != 2 {
-				t.Errorf("Expected 2 RRs in additional section, but got %d", n)
-			}
-			if r.Rcode != dns.RcodeSuccess {
-				t.Errorf("Expected success but got %d", r.Rcode)
-			}
-		})
+	if n := len(r.Answer); n != answerLength {
+		t.Fatalf("Expected %v answers, got %v", answerLength, n)
+	}
+	if n := len(r.Extra); n != 2 {
+		t.Errorf("Expected 2 RRs in additional section, but got %d", n)
+	}
+	if r.Rcode != dns.RcodeSuccess {
+		t.Errorf("Expected success but got %d", r.Rcode)
 	}
 }
