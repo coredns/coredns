c99551829347b3b1658b87e70ee4161ed0c238b5
Chris O'Haver <cohaver@infoblox.com>
2022-07-26 16:12:20 -0400

    add multi instance setup test case
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- plugin/view/setup_test.go
+++ plugin/view/setup_test.go
@@ -10,24 +10,30 @@ func TestSetup(t *testing.T) {
 	tests := []struct {
 		input     string
 		shouldErr bool
+		ruleCount int
 	}{
-		{`view name == 'example.com.'`, false},
-		{`view incidr(client_ip, '10.0.0.0/24')`, false},
-		{`view`, true},
-		{`view invalid expression`, true},
+		{`view name == 'example.com.'`, false, 1},
+		{`view incidr(client_ip, '10.0.0.0/24')`, false, 1},
+		{`view name == 'example.com.'
+view name == 'example2.com.'`, false, 2},
+		{`view`, true, 0},
+		{`view invalid expression`, true, 0},
 	}
 
 	for i, test := range tests {
-		_, err := parse(caddy.NewTestController("dns", test.input))
+		v, err := parse(caddy.NewTestController("dns", test.input))
 
 		if test.shouldErr && err == nil {
 			t.Errorf("Test %d: Expected error but found none for input %s", i, test.input)
 		}
-
-		if err != nil {
-			if !test.shouldErr {
-				t.Errorf("Test %d: Expected no error but found one for input %s. Error was: %v", i, test.input, err)
-			}
+		if err != nil && !test.shouldErr{
+			t.Errorf("Test %d: Expected no error but found one for input %s. Error was: %v", i, test.input, err)
+		}
+		if test.shouldErr {
+			continue
+		}
+		if test.ruleCount != len(v.rules) {
+			t.Errorf("Test %d: Expected rule length %d, but got %d for %s.", i, test.ruleCount, len(v.rules), test.input)
 		}
 	}
 }
