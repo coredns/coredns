b197b92e51193b9973a049d36110303107ecd234
Christoph Heer <christoph@thelabmill.de>
2022-08-09 18:52:42 +0200

    Update plugin/header/header_test.go
    Co-authored-by: Qasim Sarfraz <syed.qasim.sarfraz@gmail.com>
    Signed-off-by: Christoph Heer <christoph.heer@sap.com>

--- plugin/header/header_test.go
+++ plugin/header/header_test.go
@@ -84,18 +84,69 @@ func TestHeaderResponseRules(t *testing.T) {
 func TestHeaderQueryRules(t *testing.T) {
 	wr := dnstest.NewRecorder(&test.ResponseWriter{})
 	next := plugin.HandlerFunc(func(ctx context.Context, writer dns.ResponseWriter, msg *dns.Msg) (int, error) {
-		if !msg.RecursionDesired {
-			t.Errorf("Expected RecursionDesired")
-		}
 		writer.WriteMsg(msg)
 		return dns.RcodeSuccess, nil
 	})
 
-	handler := Header{
-		QueryRules: []Rule{{Flag: recursionDesired, State: false}},
-		Next:       next,
+	tests := []struct {
+		handler  plugin.Handler
+		got      func(msg *dns.Msg) bool
+		expected bool
+	}{
+		{
+			handler: Header{
+				QueryRules: []Rule{{Flag: recursionAvailable, State: true}},
+				Next:       next,
+			},
+			got: func(msg *dns.Msg) bool {
+				return msg.RecursionAvailable
+			},
+			expected: true,
+		},
+		{
+			handler: Header{
+				QueryRules: []Rule{{Flag: recursionDesired, State: true}},
+				Next:       next,
+			},
+			got: func(msg *dns.Msg) bool {
+				return msg.RecursionDesired
+			},
+			expected: true,
+		},
+		{
+			handler: Header{
+				QueryRules: []Rule{{Flag: recursionDesired, State: false}},
+				Next:       next,
+			},
+			got: func(msg *dns.Msg) bool {
+				return msg.RecursionDesired
+			},
+			expected: false,
+		},
+		{
+			handler: Header{
+				QueryRules: []Rule{{Flag: authoritative, State: true}},
+				Next:       next,
+			},
+			got: func(msg *dns.Msg) bool {
+				return msg.Authoritative
+			},
+			expected: true,
+		},
 	}
 
-	m := new(dns.Msg)
-	handler.ServeDNS(context.TODO(), wr, m)
+	for i, tc := range tests {
+		m := new(dns.Msg)
+
+		_, err := tc.handler.ServeDNS(context.TODO(), wr, m)
+		if err != nil {
+			t.Errorf("Test %d: Expected no error, but got %s", i, err)
+			continue
+		}
+
+		if tc.got(m) != tc.expected {
+			t.Errorf("Test %d: Expected flag state=%t, but got %t", i, tc.expected, tc.got(m))
+			continue
+		}
+	}
 }
