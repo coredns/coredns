d0ab38a54da3f08e2f1b67b467bbe3f815e7d9aa
Chris O'Haver <cohaver@infoblox.com>
2022-09-02 17:10:10 -0400

    add unit test for metadata expression function
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- plugin/pkg/expression/expression_test.go
+++ plugin/pkg/expression/expression_test.go
@@ -4,30 +4,31 @@ import (
 	"context"
 	"testing"
 
+	"github.com/coredns/coredns/plugin/metadata"
 	"github.com/coredns/coredns/request"
 )
 
 func TestInCidr(t *testing.T) {
-	incidr := DefaultEnv(context.TODO(), &request.Request{})["incidr"]
+	incidr := DefaultEnv(context.Background(), &request.Request{})["incidr"]
 
 	cases := []struct {
-		cidr1     string
-		cidr2     string
+		ip        string
+		cidr      string
 		expected  bool
 		shouldErr bool
 	}{
 		// positive
-		{cidr1: "1.2.3.4", cidr2: "1.2.0.0/16", expected: true, shouldErr: false},
-		{cidr1: "10.2.3.4", cidr2: "1.2.0.0/16", expected: false, shouldErr: false},
-		{cidr1: "1:2::3:4", cidr2: "1:2::/64", expected: true, shouldErr: false},
-		{cidr1: "A:2::3:4", cidr2: "1:2::/64", expected: false, shouldErr: false},
+		{ip: "1.2.3.4", cidr: "1.2.0.0/16", expected: true, shouldErr: false},
+		{ip: "10.2.3.4", cidr: "1.2.0.0/16", expected: false, shouldErr: false},
+		{ip: "1:2::3:4", cidr: "1:2::/64", expected: true, shouldErr: false},
+		{ip: "A:2::3:4", cidr: "1:2::/64", expected: false, shouldErr: false},
 		// negative
-		{cidr1: "1.2.3.4", cidr2: "invalid", shouldErr: true},
-		{cidr1: "invalid", cidr2: "1.2.0.0/16", shouldErr: true},
+		{ip: "1.2.3.4", cidr: "invalid", shouldErr: true},
+		{ip: "invalid", cidr: "1.2.0.0/16", shouldErr: true},
 	}
 
 	for i, c := range cases {
-		r, err := incidr.(func(string, string) (bool, error))(c.cidr1, c.cidr2)
+		r, err := incidr.(func(string, string) (bool, error))(c.ip, c.cidr)
 		if err != nil && !c.shouldErr {
 			t.Errorf("Test %d: unexpected error %v", i, err)
 			continue
@@ -39,6 +40,31 @@ func TestInCidr(t *testing.T) {
 		if c.shouldErr {
 			continue
 		}
+		if r != c.expected {
+			t.Errorf("Test %d: expected %v", i, c.expected)
+			continue
+		}
+	}
+}
+
+func TestMetadata(t *testing.T) {
+	ctx := metadata.ContextWithMetadata(context.Background())
+	metadata.SetValueFunc(ctx, "test/metadata", func() string {
+		return "success"
+	})
+	f := DefaultEnv(ctx, &request.Request{})["metadata"]
+
+	cases := []struct {
+		label     string
+		expected  string
+		shouldErr bool
+	}{
+		{label: "test/metadata", expected: "success"},
+		{label: "test/nonexistent", expected: ""},
+	}
+
+	for i, c := range cases {
+		r := f.(func(string) string)(c.label)
 		if r != c.expected {
 			t.Errorf("Test %d: expected %v", i, c.expected)
 			continue
