dca77faba2c355a446e3644cf214b299112d8de8
Erik Johansson <ejohansson@spotify.com>
2022-09-13 00:08:09 +0200

    plugin/template: add functional test to setup_test.go
    Signed-off-by: Erik Johansson <ejohansson@spotify.com>

--- plugin/template/setup_test.go
+++ plugin/template/setup_test.go
@@ -1,9 +1,14 @@
 package template
 
 import (
+	"context"
 	"testing"
 
 	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
+	"github.com/coredns/coredns/plugin/test"
+
+	"github.com/miekg/dns"
 )
 
 func TestSetup(t *testing.T) {
@@ -20,6 +25,38 @@ func TestSetup(t *testing.T) {
 	err = setupTemplate(c)
 	if err != nil {
 		t.Errorf("Expected no errors, got: %v", err)
+	}
+}
+
+func TestSetupTemplateFunction(t *testing.T) {
+	c := caddy.NewTestController("dns", `template IN A example {
+		match "^ip0a(?P<b>[a-f0-9]{2})(?P<c>[a-f0-9]{2})(?P<d>[a-f0-9]{2})[.]example[.]$"
+		answer "{{ .Name }} 60 IN A 10.{{ parseInt .Group.b 16 8 }}.{{ parseInt .Group.c 16 8 }}.{{ parseInt .Group.d 16 8 }}"
+	}`)
+
+	c.ServerBlockKeys = []string{".:8053"}
+
+	templates, err := templateParse(c)
+
+	if err != nil {
+		t.Fatalf("Unexpected error parsing template %q", err)
+	}
+
+	r := &dns.Msg{Question: []dns.Question{{Name: "ip0a5f0c09.example.", Qclass: dns.ClassINET, Qtype: dns.TypeA}}}
+	rec := dnstest.NewRecorder(&test.ResponseWriter{})
+
+	_, err = templates.ServeDNS(context.TODO(), rec, r)
+	if err != nil {
+		t.Fatalf("Unexpected error serving DNS %q", err)
+	}
+	if rec.Msg.Rcode == dns.RcodeServerFailure {
+		t.Fatalf("Rcode should not be dns.RcodeServerFailure")
+	}
+	if len(rec.Msg.Answer) != 1 {
+		t.Fatalf("Expected 1 answer, got %v", len(rec.Msg.Answer))
+	}
+	if x := rec.Msg.Answer[0].(*dns.A).A.String(); x != "10.95.12.9" {
+		t.Fatalf("Failed to get address for A record, expected 10.95.12.9 got %s", x)
 	}
 }
 
