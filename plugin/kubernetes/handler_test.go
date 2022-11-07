c679367f570c90423fe97dc659c2ed390d16a902
Laurence Robinson <laurence_robinson@live.co.uk>
2022-11-07 09:12:55 -0500

    fix test
    Signed-off-by: Laurence Robinson <laurence_robinson@live.co.uk>

--- plugin/kubernetes/handler_test.go
+++ plugin/kubernetes/handler_test.go
@@ -251,7 +251,9 @@ var dnsTestCases = []kubeTestCase{
 	{Case: test.Case{
 		Qname: "kubernetes.default.svc.cluster.local.", Qtype: dns.TypeTXT,
 		Rcode:  dns.RcodeSuccess,
-		Answer: []dns.RR{},
+		Answer: []dns.RR{
+			test.SOA("cluster.local.	5	IN	SOA	ns.dns.cluster.local. hostmaster.cluster.local. 1499347823 7200 1800 86400 5"),
+        },
 	}},
 	// A TXT record does not exist and neither does another record for the same FQDN
 	{Case: test.Case{
