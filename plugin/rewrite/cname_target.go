08cc49fe9f5d1919a44c36b6fa3462b011836180
amila <amila.15@cse.mrt.ac.lk>
2023-02-05 16:46:28 +0530

    upstream request
    Signed-off-by: amila <amila.15@cse.mrt.ac.lk>

--- plugin/rewrite/cname_target.go
+++ plugin/rewrite/cname_target.go
@@ -3,21 +3,31 @@ package rewrite
 import (
 	"context"
 	"fmt"
+	"net"
 	"strings"
 
 	"github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/coredns/coredns/plugin/pkg/upstream"
 	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
 )
+
+// UpstreamInt wraps the Upstream API for dependency injection during testing
+type UpstreamInt interface {
+	Lookup(ctx context.Context, state request.Request, name string, typ uint16) (*dns.Msg, error)
+}
 
 // cNameResponseRule is cname target rewrite rule.
 type cnameResponseRule struct {
 	fromTarget string
 	toTarget   string
 	nextAction string
+	state      request.Request
+	ctx        context.Context
+	Upstream   UpstreamInt // Upstream for looking up external names during the resolution process.
 }
 
-func (r *cnameResponseRule) RewriteResponse(rr dns.RR) {
+func (r *cnameResponseRule) RewriteResponse(res *dns.Msg, rr dns.RR) {
 	// logic to rewrite the cname target of dns response
 	switch rr.Header().Rrtype {
 	case dns.TypeCNAME:
@@ -28,7 +38,21 @@ func (r *cnameResponseRule) RewriteResponse(rr dns.RR) {
 			}
 		}
 	case dns.TypeA:
+		// TODO: Get the new answer from upstream call, replace the A records in the response
 		// remove all exising A records and pack new A records got for toTarget name
+		log.Infof("Sending upstream request...")
+		resp, err := r.Upstream.Lookup(r.ctx, r.state, r.toTarget, dns.TypeA)
+		if err == nil {
+			log.Infof("Error upstream request %v", resp)
+		} else {
+			log.Infof("Error upstream request %v", err)
+		}
+
+		if a, ok := rr.(*dns.A); ok {
+			a.Header().Name = r.toTarget
+			a.A = net.IPv4(124, 43, 66, 43) // *targetIP
+		}
+
 	}
 }
 
@@ -40,7 +64,12 @@ func newCNAMERule(nextAction string, args ...string) (Rule, error) {
 	} else {
 		return nil, fmt.Errorf("too few (%d) arguments for a cname rule", len(args))
 	}
-	rule := cnameResponseRule{fromTarget, toTarget, nextAction}
+	rule := cnameResponseRule{
+		fromTarget: fromTarget,
+		toTarget:   toTarget,
+		nextAction: nextAction,
+		Upstream:   upstream.New(),
+	}
 	log.Infof("cname rule created rule data %v'", rule)
 	return &rule, nil
 }
@@ -49,6 +78,8 @@ func newCNAMERule(nextAction string, args ...string) (Rule, error) {
 func (rule *cnameResponseRule) Rewrite(ctx context.Context, state request.Request) (ResponseRules, Result) {
 	if len(rule.fromTarget) > 0 && len(rule.toTarget) > 0 {
 		// Create rule only if question is A record
+		rule.state = state
+		rule.ctx = ctx
 		if state.QType() == dns.TypeA {
 			return ResponseRules{rule}, RewriteDone
 		}
