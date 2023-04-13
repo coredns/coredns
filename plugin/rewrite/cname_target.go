357682d888d91703b8c50e7f7ee9a440ff1d58eb
amila <amila.15@cse.mrt.ac.lk>
2023-04-13 11:55:01 +0530

    comment fixed
    Signed-off-by: amila <amila.15@cse.mrt.ac.lk>

--- plugin/rewrite/cname_target.go
+++ plugin/rewrite/cname_target.go
@@ -88,7 +88,7 @@ func (r *cnameTargetRule) RewriteResponse(res *dns.Msg, rr dns.RR) {
 						newAnswer = append(newAnswer, rr)
 					}
 				}
-				// iterate over upstream response made
+				// iterate over upstream response recieved
 				for _, rr := range upRes.Answer {
 					if rr.Header().Name == toTarget {
 						newAnswer = append(newAnswer, rr)
