2fbaad71c9c655ad6b21ce061518ddc92a74d874
Thomas Meitz <info@jproxx.com>
2023-04-28 18:11:26 +0200

    Only a dead linter is a good linter
    Signed-off-by: Thomas Meitz <info@jproxx.com>

--- plugin/sql/ent/schema/dnszone.go
+++ plugin/sql/ent/schema/dnszone.go
@@ -126,7 +126,7 @@ func (DnsZone) Edges() []ent.Edge {
 	}
 }
 
-// Index the columns
+// Indexes the columns
 func (DnsZone) Indexes() []ent.Index {
 	return []ent.Index{
 		index.Fields("activated"),
