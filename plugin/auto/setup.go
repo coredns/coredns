37bd613e53bf893c8e0696d116a6f80ee0a59a6c
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-02-09 11:27:26 +0100

    chore: enable early-return and superfluous-else from revive
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- plugin/auto/setup.go
+++ plugin/auto/setup.go
@@ -118,11 +118,10 @@ func autoParse(c *caddy.Controller) (Auto, error) {
 				}
 				_, err := os.Stat(a.directory)
 				if err != nil {
-					if os.IsNotExist(err) {
-						log.Warningf("Directory does not exist: %s", a.directory)
-					} else {
-						return a, c.Errf("Unable to access root path '%s': %v", a.directory, err)
+					if !os.IsNotExist(err) {
+						return a, c.Errf("Unable to access root path '%s': %v", a.loader.directory, err)
 					}
+					log.Warningf("Directory does not exist: %s", a.loader.directory)
 				}
 
 				// regexp template
