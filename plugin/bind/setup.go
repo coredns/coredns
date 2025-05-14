coredns/coredns

commit 6efa95ca98697264a4eeae63ef70c345cb711a67

plugin/bind: add zone for link-local IPv6 instead of skipping (#6547)
* add zone for link-local IPv6 instead of skipping

Signed-off-by: Till Riedel <riedel@teco.edu>

* revert changed error handling for IPs

Signed-off-by: Till Riedel <riedel@teco.edu>

---------

Signed-off-by: Till Riedel <riedel@teco.edu>

plugin/bind/setup.go
@@ -84,8 +84,15 @@ func listIP(args []string, ifaces []net.Interface) ([]string, error) {
 				}
 				for _, addr := range addrs {
 					if ipnet, ok := addr.(*net.IPNet); ok {
-						if ipnet.IP.To4() != nil || (!ipnet.IP.IsLinkLocalMulticast() && !ipnet.IP.IsLinkLocalUnicast()) {
-							all = append(all, ipnet.IP.String())
+						ipa, err := net.ResolveIPAddr("ip", ipnet.IP.String())
+						if err == nil {
+							if len(ipnet.IP) == net.IPv6len &&
+								(ipnet.IP.IsLinkLocalMulticast() || ipnet.IP.IsLinkLocalUnicast()) {
+								if ipa.Zone == "" {
+									ipa.Zone = iface.Name
+								}
+							}
+							all = append(all, ipa.String())
 						}
 					}
 				}

commit 19a4c2b888f68006e107c3d500bd1f7d1e0c6664

fix(plugin/bind): remove zone for link-local IPv4
This fixes a bug introduced in
https://github.com/coredns/coredns/pull/6547 which resulted in the zone
being added to IPv4 addresses.  This bug results in a failure to start
when binding to an interface with a link-local IPv4 address assigned to
it, with the following error:

    $ ./coredns -conf=/etc/coredns/Corefile
    maxprocs: Leaving GOMAXPROCS=4: CPU quota undefined
    lookup 169.254.1.1%dummy0: no such host

plugin/bind/setup.go
@@ -85,7 +85,7 @@ func listIP(args []string, ifaces []net.Interface) ([]string, error) {
 					if ipnet, ok := addr.(*net.IPNet); ok {
 						ipa, err := net.ResolveIPAddr("ip", ipnet.IP.String())
 						if err == nil {
-							if len(ipnet.IP) == net.IPv6len &&
+							if ipnet.IP.To4() == nil &&
 								(ipnet.IP.IsLinkLocalMulticast() || ipnet.IP.IsLinkLocalUnicast()) {
 								if ipa.Zone == "" {
 									ipa.Zone = iface.Name