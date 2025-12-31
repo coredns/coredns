91730db6ffae86e2dfe7862f9e1d49f17f1fc972
Azeez Syed <syedazeez337@gmail.com>
2025-12-31 11:11:25 +0530

    feat(proxy): add upstream_response_duration_seconds metric
    This adds a new Prometheus histogram metric that measures the precise time
    taken between when a DNS request is sent to the upstream server and when
    a response is received, excluding any CoreDNS-internal delays.
    
    Fixes: https://github.com/coredns/coredns/issues/7382
    
    Co-authored-by: Ben Kochie <superq@gmail.com>
    Signed-off-by: Azeez Syed <syedazeez337@gmail.com>

--- plugin/pkg/proxy/connect.go
+++ plugin/pkg/proxy/connect.go
@@ -141,6 +141,8 @@ func (p *Proxy) Connect(ctx context.Context, state request.Request, opts Options
 		return nil, err
 	}
 
+	upstreamStart := time.Now()
+
 	var ret *dns.Msg
 	pc.c.SetReadDeadline(time.Now().Add(p.readTimeout))
 	for {
@@ -173,6 +175,7 @@ func (p *Proxy) Connect(ctx context.Context, state request.Request, opts Options
 			break
 		}
 	}
+
 	// recovery the origin Id after upstream.
 	ret.Id = originId
 
@@ -182,6 +185,8 @@ func (p *Proxy) Connect(ctx context.Context, state request.Request, opts Options
 	if !ok {
 		rc = strconv.Itoa(ret.Rcode)
 	}
+
+	upstreamResponseDuration.WithLabelValues(p.addr, proto, rc, p.proxyName).Observe(time.Since(upstreamStart).Seconds())
 
 	requestDuration.WithLabelValues(p.proxyName, p.addr, rc).Observe(time.Since(start).Seconds())
 
